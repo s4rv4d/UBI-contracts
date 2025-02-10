7da9eea8b1bc5d7fe49b45cd1158c4dbf7363fc7
serverConnected <sarvadshetty@gmail.com>
2025-02-10 16:52:42 +0530

    created UBISplitProxy - UUPS

---
+++ src/UBISplitProxy.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.22;
+
+import {ERC1967Proxy} from "openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+contract UBISplitProxy is ERC1967Proxy {
+
+    /// constructor
+    constructor(address _implementation, bytes memory _data) ERC1967Proxy(_implementation, _data) {
+
+    }
+}
